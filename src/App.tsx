7b4d0d2ef55e2aaadd8b6c1e8b5aa9a3ac5f71d3
Polina <polinawakeup@yandex.ru>
2025-07-23 15:06:24 +0200

    Task completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,153 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+interface Entry {
+  date: string;
+  distance: string;
+}
+
+function App(): React.ReactElement {
+
+  const [date, setDate] = useState('');
+  const [distance, setDistance] = useState('');
+  const [data, setData] = useState<Entry[]>([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editId, setEditId] = useState('');
+
+  function handleDateChange ( {target}: React.ChangeEvent<HTMLInputElement> ): void {
+    setDate(target.value);
+  }
+
+  function handleDistanceChange ( {target}: React.ChangeEvent<HTMLInputElement> ): void {
+    setDistance(target.value);
+  }
+  
+  function handleSubmit (e: React.ChangeEvent<HTMLFormElement>): void {
+    e.preventDefault();
+
+    if (date === '' || distance === '') {
+      console.log('Пустое поле');
+      return
+    }
+    
+    const newEntry: Entry = {
+      date,
+      distance
+    }
+
+    if(editMode) {
+      setData(prevData =>
+        prevData.map(entry =>
+          entry.date === editId
+          ? { date, distance }
+          : entry
+        )
+      );
+      setEditId('');
+      setEditMode(false);
+    } else {
+      setData((prevData) => {
+      const index = (prevData.findIndex(entry => entry.date === newEntry.date));
+
+      if (index !== -1) {
+        return prevData.map((entry, i) =>
+          i === index
+          ? {
+              ...entry,
+              distance: (
+                parseInt(newEntry.distance) + parseInt(prevData[index].distance)
+              ).toString()
+            }
+          : entry
+        )
+      }
+        
+      return [...prevData, newEntry];
+      });
+    }
+
+    setDate('');
+    setDistance('');
+  }
+
+  function formatDate(dateStr: string): string {
+    const date = new Date(dateStr);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear().toString();
+    return `${day}.${month}.${year}`
+  }
+
+  function handleDelete(dateId: string): void {
+    setData(prevData => prevData.filter(entry => entry.date !== dateId));
+  }
+
+  function handleEdit(dateId: string): void {
+    const targetEntry = data.find(entry => entry.date === dateId);
+    if (targetEntry) {
+      setDate(targetEntry.date);
+      setDistance(targetEntry.distance);
+      setEditMode(true);
+      setEditId(targetEntry.date);
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+    <div className='container'>
+      <form onSubmit={handleSubmit}>
+        <div className='input_wrapper'>
+          <label htmlFor="date">Дата (ДД.ММ.ГГ)</label>
+          <input
+            type="date"
+            id='date' name='date'
+            value={date}
+            onChange={handleDateChange} />
+        </div>
+        <div className='input_wrapper'>
+          <label htmlFor="distance">Пройдено км</label>
+          <input
+            type="number"
+            id='distance' name='distance'
+            value={distance}
+            onChange={handleDistanceChange} />
+        </div>
+        <button type='submit'>
+          {!editMode ? "ОК" : "Сохранить"}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      </form>
+      <div className='tableRow'>
+        <div className='row header'>
+          <span className='cell'>Дата (ДД.ММ.ГГ)</span>
+          <span className='cell'>Пройдено км</span>
+          <span className='cell'>Действия</span>
+        </div>
+        {data.length !== 0
+          ? (data.slice().sort((a, b) => 
+              new Date(b.date).getTime() - new Date(a.date).getTime())
+            ).map(entry => {
+              return (
+                <div className='row' key={entry.date}>
+                  <span className='cell'>
+                    {formatDate(entry.date)}
+                  </span>
+                  <span className='cell'>{entry.distance}</span>
+                  <div className='cell controls'>
+                    <button
+                      className='icon'
+                      onClick={() => handleDelete(entry.date)}
+                    >delete</button>
+                    <button
+                      className='icon'
+                      onClick={() => handleEdit(entry.date)}
+                    >edit</button>
+                  </div>
+                </div>
+              )
+            })
+          : null
+        }
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
